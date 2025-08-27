03295f38c55f04e87c3f10138c41c00bff0bb672
Thathsara-Dassanayake <dmtkr.dassanayake@gmail.com>
2025-08-27 13:41:04 +0000

    finalizing code

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,6 +1,6 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import * as fs from 'fs';
+//import * as fs from 'fs';
 
 
 // https://vite.dev/config/
@@ -8,15 +8,15 @@ export default defineConfig({
     plugins: [react()],
 
     server: {
-        https: {
-            cert: fs.readFileSync('cert.pem'),
-            key: fs.readFileSync('key.pem'),
-        },
+        // https: {
+        //     cert: fs.readFileSync('cert.pem'),
+        //     key: fs.readFileSync('key.pem'),
+        // },
         proxy: {
             // when frontend calls `/api/...`, proxy it to your backend
             
             '/api': {
-                target: 'http://localhost:8080',
+                target: 'https://jubilant-umbrella-4jgxj65g9r5gf799g-8080.app.github.dev',
                 changeOrigin: true,
             },
         },
