56f74c07e698de0dc6edd95ecc17aee9bd96a1ef
Kisal Thennakoon <156928595+kisalthennakoon@users.noreply.github.com>
2025-10-04 16:16:06 +0000

    Seeding algorithm

--- backend/src/main/java/com/devix/backend/model/Inspection.java
+++ backend/src/main/java/com/devix/backend/model/Inspection.java
@@ -4,6 +4,7 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
@@ -24,4 +25,14 @@ public class Inspection {
     private String inspectionStatus;
     private String transformerNo;
     private String inspectedby;
+
+    public Inspection(String inspectionNo, String inspectionDate, String inspectionTime, String inspectionBranch, String inspectionStatus, String transformerNo, String inspectedby) {
+        this.inspectionNo = inspectionNo;
+        this.inspectionDate = inspectionDate;
+        this.inspectionTime = inspectionTime;
+        this.inspectionBranch = inspectionBranch;
+        this.inspectionStatus = inspectionStatus;
+        this.transformerNo = transformerNo;
+        this.inspectedby = inspectedby;
+    }
 }
